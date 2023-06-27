8247a3bcacd2d3ea24d110fa81e790040f269fd6
Fabio Araujo <fabioaraujods@gmail.com>
2023-06-26 23:40:20 -0300

    incluído calculo da curva de juros planejada

--- views.py
+++ views.py
@@ -1,5 +1,7 @@
 from flask import request
 import flask
+from datetime import datetime
+
 from app import app, db
 from models import Sonho, SonhoAcompanhamento
 
@@ -83,5 +85,51 @@ def sonho_detalhes_json(id):
     line = [["data", "valor"]]
     for ac in acompanhamentos:
         line.append([str(ac.data)[:10], ac.valor])
+
+    return line
+
+
+def calcular_taxa_juros_anual(
+    valor_inicial, data_inicial, valor_final, data_final, aporte_anual
+):
+    # Cálculo do número total de dias entre as datas
+    dias_totais = (data_final - data_inicial).days
+
+    # Cálculo da taxa de juros anual iterativamente
+    taxa_juros = 0.01  # Valor inicial para a taxa de juros (1%)
+    precisao = 0.0001  # Precisão desejada
+    valor_atual = valor_inicial
+
+    while valor_atual < valor_final:
+        valor_atual = valor_inicial
+        for ano in range(1, (dias_totais // 365) + 1):
+            valor_atual += aporte_anual
+            valor_atual *= 1 + taxa_juros
+
+        if valor_atual < valor_final:
+            taxa_juros += precisao
+
+    return taxa_juros
+
+
+@app.route("/sonho_planejado_json/<int:id>", methods=["GET"])
+def sonho_planejado_json(id):
+    sonho = Sonho.query.filter_by(id=id).first()
+
+    line = [["data", "valor"]]
+    data = sonho.data_inicial
+    valor = sonho.valor_inicial
+    if valor == 0:
+        valor = 1
+    aporte_anual = 24000
+    taxa_juros = calcular_taxa_juros_anual(
+        valor, sonho.data_inicial, sonho.valor_final, sonho.data_final, aporte_anual
+    )
+    print(taxa_juros)
+    line.append([str(data)[:10], valor])
+    while data <= sonho.data_final:
+        data = datetime(data.year + 1, data.month, data.day)
+        valor += valor * taxa_juros + aporte_anual
+        line.append([str(data)[:10], valor])
 
     return line
